c54829953c4257a54176cfc41ca723e8d2c8913e
Aleksandr Zhernovoy <gernowoy@gmail.com>
2024-01-24 20:09:19 +0300

    feature: add types

---
+++ src/types/types.ts
@@ -0,0 +1,44 @@
+import { categories } from "../utils/constants"
+
+const categoryNames = categories.map(category => category.name);
+export type CategoryName = typeof categoryNames[number];
+
+export interface SidebarProps {
+  selectedCategory: CategoryName;
+  setSelectedCategory: (category: CategoryName) => void;
+}
+
+export interface Video {
+  kind:    string;
+  id:      ID;
+  snippet: Snippet;
+}
+
+export interface ID {
+  kind:    string;
+  videoId?: string;
+  channelId?: string;
+}
+
+export interface Snippet {
+  publishedAt:          Date;
+  channelId:            string;
+  title:                string;
+  description:          string;
+  thumbnails:           Thumbnails;
+  channelTitle:         string;
+  liveBroadcastContent: string;
+  publishTime:          Date;
+}
+
+export interface Thumbnails {
+  default: Default;
+  medium:  Default;
+  high:    Default;
+}
+
+export interface Default {
+  url:    string;
+  width:  number;
+  height: number;
+}
